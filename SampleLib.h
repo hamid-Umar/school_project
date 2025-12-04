ed437781d4669fa2c9fbb45e934f5f2acd742d75
hamid-Umar <ohamidou683@gmail.com>
2025-12-04 03:27:46 +0100

    Premier commit

---
+++ SampleLib.h
@@ -0,0 +1,7 @@
+#ifndef MY_MATH_LIB_H
+#define MY_MATH_LIB_H
+// Définition d'une fonction de calcul de factorielle.
+unsigned int fact( unsigned int value );
+// Définition d'une fonction d'élévation à une puissance données.
+int power( int value, unsigned int pow );
+#endif
